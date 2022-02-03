946d0f3a2d1a6ed908ebca6cf9aaf113561c86b5
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2022-02-03 14:54:24 +0530

    Feat : UI Added in regular home page

--- src/app/home/index/index.component.ts
+++ src/app/home/index/index.component.ts
@@ -8,9 +8,113 @@ import { Router } from '@angular/router';
 })
 export class IndexComponent implements OnInit {
 
+  userList = [
+    {
+      imageUrl : '../../../assets/user1.jpeg',
+      name: 'santhoshkumar',
+      age: '27',
+      height: "5'2",
+      educate: 'BE',
+      caste: 'chettier',
+      state: 'Pondicherry',
+      country: 'India'
+    },
+    {
+      imageUrl : '../../../assets/user2.jpeg',
+      name: 'santhoshkumar',
+      age: '27',
+      height: "5'2",
+      educate: 'BE',
+      caste: 'chettier',
+      state: 'Pondicherry',
+      country: 'India'
+    },
+    {
+      imageUrl : '../../../assets/user3.jpeg',
+      name: 'santhoshkumar',
+      age: '27',
+      height: "5'2",
+      educate: 'BE',
+      caste: 'chettier',
+      state: 'Pondicherry',
+      country: 'India'
+    },
+    {
+      imageUrl : '../../../assets/user4.jpeg',
+      name: 'santhoshkumar',
+      age: '27',
+      height: "5'2",
+      educate: 'BE',
+      caste: 'chettier',
+      state: 'Pondicherry',
+      country: 'India'
+    },
+    {
+      imageUrl : '../../../assets/user5.jpeg',
+      name: 'santhoshkumar',
+      age: '27',
+      height: "5'2",
+      educate: 'BE',
+      caste: 'chettier',
+      state: 'Pondicherry',
+      country: 'India'
+    },
+    {
+      imageUrl : '../../../assets/user6.jpeg',
+      name: 'santhoshkumar',
+      age: '27',
+      height: "5'2",
+      educate: 'BE',
+      caste: 'chettier',
+      state: 'Pondicherry',
+      country: 'India'
+    },
+    {
+      imageUrl : '../../../assets/user7.jpeg',
+      name: 'santhoshkumar',
+      age: '27',
+      height: "5'2",
+      educate: 'BE',
+      caste: 'chettier',
+      state: 'Pondicherry',
+      country: 'India'
+    },
+    {
+      imageUrl : '../../../assets/user8.jpeg',
+      name: 'santhoshkumar',
+      age: '27',
+      height: "5'2",
+      educate: 'BE',
+      caste: 'chettier',
+      state: 'Pondicherry',
+      country: 'India'
+    },
+    {
+      imageUrl : '../../../assets/user9.jpeg',
+      name: 'santhoshkumar',
+      age: '27',
+      height: "5'2",
+      educate: 'BE',
+      caste: 'chettier',
+      state: 'Pondicherry',
+      country: 'India'
+    },
+    {
+      imageUrl : '../../../assets/user10.jpeg',
+      name: 'santhoshkumar',
+      age: '27',
+      height: "5'2",
+      educate: 'BE',
+      caste: 'chettier',
+      state: 'Pondicherry',
+      country: 'India'
+    },
+  ] 
+    
   constructor(
     private router: Router
-  ) { }
+  ) 
+  { }
 
   ngOnInit(): void {
   }
